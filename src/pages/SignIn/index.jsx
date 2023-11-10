85e68f8f9b40bfc96c25140f57d20d3fe9575019
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2023-11-10 01:09:25 -0300

    feat: add SignIn and SignUp pages with styles

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
+import "./signIn.scss";
+
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassWord] = useState("");
+
   return (
-    <div>
-      <h1>SignIn</h1>
+    <div className="login-area">
+      <div className="card">
+        <img src={logo} alt="Logo Gm solutions" />
+
+        <span>Login</span>
+        <p>Use sua conta Gm Solutions</p>
+
+        <form>
+          <div className="input-container">
+            <input
+              type="text"
+              id="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+
+          <div className="input-container">
+            <input
+              type="password"
+              id="password"
+              required
+              value={password}
+              onChange={(e) => setPassWord(e.target.value)}
+            />
+            <label htmlFor="password">Senha</label>
+          </div>
+
+          <div className="actions-area">
+            <Link to="/register">Criar uma conta</Link>
+            <button type="submit" className="login">
+              Entrar
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,69 @@
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
 function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassWord] = useState("");
+
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className="login-area">
+      <div className="card">
+        <img src={logo} alt="Logo Gm solutions" />
+        <span style={{ marginBottom: "0" }}>Criar nova conta</span>
+
+        <form>
+          <div className="input-container">
+            <input
+              type="text"
+              id="name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label htmlFor="email">Nome</label>
+          </div>
+
+          <div className="input-container">
+            <input
+              type="text"
+              id="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+
+          <div className="input-container">
+            <input
+              type="password"
+              id="password"
+              required
+              value={password}
+              onChange={(e) => setPassWord(e.target.value)}
+            />
+            <label htmlFor="password">Senha</label>
+          </div>
+
+          <div className="actions-area">
+            <button type="submit" className="login">
+              Cadastrar
+            </button>
+            <Link
+              to="/"
+              style={{
+                width: "100%",
+                textAlign: "center",
+                marginTop: "3.6rem",
+              }}
+            >
+              Já possui uma conta? Faça login
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
