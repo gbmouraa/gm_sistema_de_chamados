cb0adf26c13dae39de80d1d8e036f94ef6a548c6
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2023-12-02 17:54:08 -0300

    feat: add handleFile function

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,22 +1,28 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import { Settings, Pencil, UserRound } from "lucide-react";
 import { AuthContext } from "../../contexts/auth";
-
-import userPic from "../../assets/userCircle.png";
-import test from "../../assets/154dc172-9dc7-4c97-be8d-81bbb30a353a[421].jpg";
 
 import "./profile.scss";
 
 function Profile() {
   const { user } = useContext(AuthContext);
 
+  // efeito visual instataneo
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+  // para alterar no banco
+  const [imageAvatar, setImageAvatar] = useState(null);
+
   function handleFile(e) {
     const img = e.target.files[0];
 
     if (img.type === "image/jpeg" || img.type === "image/png") {
-      /// continuar aqui
+      setImageAvatar(img);
+
+      setAvatarUrl(URL.createObjectURL(img));
+    } else {
+      alert("Insira uma imagem do tipo jpeg ou png.");
     }
   }
 
@@ -34,10 +40,10 @@ function Profile() {
           <section>
             <div className="form-profile">
               <div className="avatar-container">
-                {user.avatarUrl === null ? (
+                {avatarUrl === null ? (
                   <UserRound size={180} color="#ccc" />
                 ) : (
-                  <img src={user.avatarUrl} alt="Foto de perfil" />
+                  <img src={avatarUrl} alt="Foto de perfil" />
                 )}
 
                 <label htmlFor="input-file">
