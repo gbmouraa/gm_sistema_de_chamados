d6a805f2560e642ed083dae917683cc3dd3a9745
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2023-12-06 22:26:36 -0300

    feat: add html of Dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,12 +1,14 @@
 import { AuthContext } from "../../contexts/auth";
 import { useContext } from "react";
 import Header from "../../components/Header";
+import Title from "../../components/Title";
 import ModalLogout from "../../components/ModalLogout";
+import { MessagesSquare, Search, Pencil } from "lucide-react";
 
 import "./dashboard.scss";
 
 function Dashboard() {
-  const { logOut, showNav, setShowNav } = useContext(AuthContext);
+  const { setShowNav, showModalLogout } = useContext(AuthContext);
 
   return (
     <div className="container">
@@ -14,10 +16,88 @@ function Dashboard() {
 
       <main onClick={() => setShowNav(false)}>
         <div className="content">
-          <h1>Página Dashboard</h1>
-          <button onClick={logOut}>Sair</button>
+          <Title>
+            <MessagesSquare size={30} color="#ccc" />
+            Chamados
+          </Title>
+
+          <div className="area-btn-add-chamados">
+            <button>Teste</button>
+          </div>
+
+          <section style={{ padding: "0" }}>
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Data de cadastro</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Loja1</td>
+                  <td data-label="Assunto">Visita tecnica</td>
+                  <td data-label="Status">
+                    <span>Progresso</span>
+                  </td>
+                  <td data-label="Data de cadastro">17/11/2022</td>
+
+                  <td>
+                    <span>
+                      <Search color="#fff" />
+                    </span>
+                    <span>
+                      <Pencil color="#fff" />
+                    </span>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td data-label="Cliente">Loja2</td>
+                  <td data-label="Assunto">Financeiro</td>
+                  <td data-label="Status">
+                    <span>Aberto</span>
+                  </td>
+                  <td data-label="Data de cadastro">17/11/2022</td>
+
+                  <td>
+                    <span>
+                      <Search color="#fff" />
+                    </span>
+                    <span>
+                      <Pencil color="#fff" />
+                    </span>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td data-label="Cliente">Loja3</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span>Concluido</span>
+                  </td>
+                  <td data-label="Data de cadastro">17/11/2022</td>
+
+                  <td>
+                    <span className="search">
+                      <Search color="#fff" />
+                    </span>
+                    <span className="edit">
+                      <Pencil color="#fff" />
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
         </div>
       </main>
+
+      {showModalLogout && <ModalLogout />}
     </div>
   );
 }
