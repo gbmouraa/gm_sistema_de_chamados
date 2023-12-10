75bdab18641f9971a613b449fec84c594bf02461
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2023-12-10 12:13:54 -0300

    feat: add loadChamados and updateStates (test)

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,15 +1,64 @@
 import { AuthContext } from "../../contexts/auth";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import { MessagesSquare, Search, Pencil } from "lucide-react";
+import { db } from "../../services/firebaseConection";
+import {
+  collection,
+  getDocs,
+  orderBy,
+  startAfter,
+  query,
+  limit,
+  getDoc,
+} from "firebase/firestore";
 
 import { Link } from "react-router-dom";
 
 import "./dashboard.scss";
+import { format } from "date-fns";
+
+const listRef = collection(db, "chamados");
 
 function Dashboard() {
   const { setShowNav } = useContext(AuthContext);
+
+  const [chamados, setChamados] = useState([]);
+
+  useEffect(() => {
+    const loadChamados = async () => {
+      const q = query(listRef, orderBy("created", "desc"), limit(5));
+
+      const querySnapShot = await getDocs(q);
+      await updateState(querySnapShot);
+    };
+
+    setChamados([]);
+    loadChamados();
+
+    return () => {};
+  }, []);
+
+  async function updateState(querySnapShot) {
+    if (querySnapShot.size === 0) return;
+
+    const lista = [];
+
+    querySnapShot.forEach((doc) => {
+      lista.push({
+        createdFormat: format(doc.data().created.toDate(), "dd/MM/yyyy"),
+        cliente: doc.data().cliente,
+        assunto: doc.data().assunto,
+        status: doc.data().status,
+        complemento: doc.data().complemento,
+        id: doc.data().id,
+        userId: doc.data().userId,
+      });
+    });
+
+    setChamados((chamados) => [...chamados, ...lista]);
+  }
 
   return (
     <div className="container">
@@ -39,61 +88,31 @@ function Dashboard() {
               </thead>
 
               <tbody>
-                <tr>
-                  <td data-label="Cliente">Loja1</td>
-                  <td data-label="Assunto">Visita tecnica</td>
-                  <td data-label="Status">
-                    <span>Progresso</span>
-                  </td>
-                  <td data-label="Data de cadastro">17/11/2022</td>
+                {chamados.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td data-label="Cliente">{item.cliente}</td>
+                      <td data-label="Assunto">{item.assunto}</td>
+                      <td data-label="Status">
+                        <span>{item.status}</span>
+                      </td>
+                      <td data-label="Data de cadastro">
+                        {item.createdFormat}
+                      </td>
 
-                  <td>
-                    <button className="search">
-                      <Search size={20} color="#fff" />
-                    </button>
-                    <button className="edit">
-                      <Pencil size={20} color="#fff" />
-                    </button>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td data-label="Cliente">Loja2</td>
-                  <td data-label="Assunto">Financeiro</td>
-                  <td data-label="Status">
-                    <span>Aberto</span>
-                  </td>
-                  <td data-label="Data de cadastro">17/11/2022</td>
-
-                  <td>
-                    <button className="search">
-                      <Search size={20} color="#fff" />
-                    </button>
-                    <button className="edit">
-                      <Pencil size={20} color="#fff" />
-                    </button>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td data-label="Cliente">Loja3</td>
-                  <td data-label="Assunto">Suporte</td>
-                  <td data-label="Status">
-                    <span>Concluido</span>
-                  </td>
-                  <td data-label="Data de cadastro">17/11/2022</td>
-
-                  <td>
-                    <button className="search">
-                      {/* abrir modal co detalhes do chamado */}
-                      <Search size={20} color="#fff" />
-                    </button>
-                    <button className="edit">
-                      {/* abrir pagina para editar chamado, pegar id do chamado, usar Link                      */}
-                      <Pencil size={20} color="#fff" />
-                    </button>
-                  </td>
-                </tr>
+                      <td>
+                        <div className="actions-table">
+                          <Link className="search">
+                            <Search size={20} color="#fff" />
+                          </Link>
+                          <Link className="edit">
+                            <Pencil size={20} color="#fff" />
+                          </Link>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </section>
